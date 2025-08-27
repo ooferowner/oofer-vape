a5eef55733044f9a4275ba10fd09b42374ec42a8
ooferowner <hamzanoor302013@gmail.com>
2025-08-27 15:38:28 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -12,13 +12,13 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/ooferowner/oofer-vape/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/ooferowner/oofer-vape/'..readfile('oofer-vape/profiles/commit.txt')..'/'..select(1, path:gsub('oofer%-vape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
 		end
 		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after oofer-vape updates.\n'..res
 		end
 		writefile(path, res)
 	end
@@ -29,32 +29,32 @@ local function wipeFolder(path)
 	if not isfolder(path) then return end
 	for _, file in listfiles(path) do
 		if file:find('loader') then continue end
-		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after oofer-vape updates.')) == 1 then
 			delfile(file)
 		end
 	end
 end
 
-for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
+for _, folder in {'oofer-vape', 'oofer-vape/games', 'oofer-vape/profiles', 'oofer-vape/assets', 'oofer-vape/libraries', 'oofer-vape/guis'} do
 	if not isfolder(folder) then
 		makefolder(folder)
 	end
 end
 
-if not shared.VapeDeveloper then
+if not shared.OoferDeveloper then
 	local _, subbed = pcall(function()
 		return game:HttpGet('https://github.com/ooferowner/oofer-vape')
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
 	commit = commit and #commit == 40 and commit or 'main'
-	if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
-		wipeFolder('newvape')
-		wipeFolder('newvape/games')
-		wipeFolder('newvape/guis')
-		wipeFolder('newvape/libraries')
+	if commit == 'main' or (isfile('oofer-vape/profiles/commit.txt') and readfile('oofer-vape/profiles/commit.txt') or '') ~= commit then
+		wipeFolder('oofer-vape')
+		wipeFolder('oofer-vape/games')
+		wipeFolder('oofer-vape/guis')
+		wipeFolder('oofer-vape/libraries')
 	end
-	writefile('newvape/profiles/commit.txt', commit)
+	writefile('oofer-vape/profiles/commit.txt', commit)
 end
 
-return loadstring(downloadFile('newvape/main.lua'), 'main')()
+return loadstring(downloadFile('oofer-vape/main.lua'), 'main')()
