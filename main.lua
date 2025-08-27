a9ffa0acb804f1f41d3020e91c90fee937d987f1
ooferowner <hamzanoor302013@gmail.com>
2025-08-27 15:37:59 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 repeat task.wait() until game:IsLoaded()
-if shared.vape then shared.vape:Uninject() end
+if shared["oofer-vape"] then shared["oofer-vape"]:Uninject() end
 
 -- why do exploits fail to implement anything correctly? Is it really that hard?
 if identifyexecutor then
@@ -8,14 +8,15 @@ if identifyexecutor then
 	end
 end
 
-local vape
+local oofer
 local loadstring = function(...)
 	local res, err = loadstring(...)
-	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+	if err and oofer then
+		oofer:CreateNotification('oofer-vape', 'Failed to load : '..err, 30, 'alert')
 	end
 	return res
 end
+
 local queue_on_teleport = queue_on_teleport or function() end
 local isfile = isfile or function(file)
 	local suc, res = pcall(function()
@@ -23,21 +24,23 @@ local isfile = isfile or function(file)
 	end)
 	return suc and res ~= nil and res ~= ''
 end
+
 local cloneref = cloneref or function(obj)
 	return obj
 end
+
 local playersService = cloneref(game:GetService('Players'))
 
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/ooferowner/oofer-vape/'..readfile('oofer-vape/profiles/commit.txt')..'/'..select(1, path:gsub('oofer%-vape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
 		end
 		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after oofer-vape updates.\n'..res
 		end
 		writefile(path, res)
 	end
@@ -45,73 +48,75 @@ local function downloadFile(path, func)
 end
 
 local function finishLoading()
-	vape.Init = nil
-	vape:Load()
+	oofer.Init = nil
+	oofer:Load()
 	task.spawn(function()
 		repeat
-			vape:Save()
+			oofer:Save()
 			task.wait(10)
-		until not vape.Loaded
+		until not oofer.Loaded
 	end)
 
 	local teleportedServers
-	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-		if (not teleportedServers) and (not shared.VapeIndependent) then
+	oofer:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
+		if (not teleportedServers) and (not shared.OoferIndependent) then
 			teleportedServers = true
 			local teleportScript = [[
-				shared.vapereload = true
-				if shared.VapeDeveloper then
-					loadstring(readfile('newvape/loader.lua'), 'loader')()
+				shared.ooferreload = true
+				if shared.OoferDeveloper then
+					loadstring(readfile('oofer-vape/loader.lua'), 'loader')()
 				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+					loadstring(game:HttpGet('https://raw.githubusercontent.com/ooferowner/oofer-vape/'..readfile('oofer-vape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
 				end
 			]]
-			if shared.VapeDeveloper then
-				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
+			if shared.OoferDeveloper then
+				teleportScript = 'shared.OoferDeveloper = true\n'..teleportScript
 			end
-			if shared.VapeCustomProfile then
-				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
+			if shared.OoferCustomProfile then
+				teleportScript = 'shared.OoferCustomProfile = "'..shared.OoferCustomProfile..'"\n'..teleportScript
 			end
-			vape:Save()
+			oofer:Save()
 			queue_on_teleport(teleportScript)
 		end
 	end))
 
-	if not shared.vapereload then
-		if not vape.Categories then return end
-		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
+	if not shared.ooferreload then
+		if not oofer.Categories then return end
+		if oofer.Categories.Main.Options['GUI bind indicator'].Enabled then
+			oofer:CreateNotification('Finished Loading', oofer.OoferButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(oofer.Keybind, ' + '):upper()..' to open GUI', 5)
 		end
 	end
 end
 
-if not isfile('newvape/profiles/gui.txt') then
-	writefile('newvape/profiles/gui.txt', 'new')
+if not isfile('oofer-vape/profiles/gui.txt') then
+	writefile('oofer-vape/profiles/gui.txt', 'new')
 end
-local gui = readfile('newvape/profiles/gui.txt')
 
-if not isfolder('newvape/assets/'..gui) then
-	makefolder('newvape/assets/'..gui)
+local gui = readfile('oofer-vape/profiles/gui.txt')
+
+if not isfolder('oofer-vape/assets/'..gui) then
+	makefolder('oofer-vape/assets/'..gui)
 end
-vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
-shared.vape = vape
 
-if not shared.VapeIndependent then
-	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
-	if isfile('newvape/games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+oofer = loadstring(downloadFile('oofer-vape/guis/'..gui..'.lua'), 'gui')()
+shared["oofer-vape"] = oofer
+
+if not shared.OoferIndependent then
+	loadstring(downloadFile('oofer-vape/games/universal.lua'), 'universal')()
+	if isfile('oofer-vape/games/'..game.PlaceId..'.lua') then
+		loadstring(readfile('oofer-vape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
 	else
-		if not shared.VapeDeveloper then
+		if not shared.OoferDeveloper then
 			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+				return game:HttpGet('https://raw.githubusercontent.com/ooferowner/oofer-vape/'..readfile('oofer-vape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 			end)
 			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+				loadstring(downloadFile('oofer-vape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
 			end
 		end
 	end
 	finishLoading()
 else
-	vape.Init = finishLoading
-	return vape
+	oofer.Init = finishLoading
+	return oofer
 end
